2b7bafc097bc156e80e35da942ad4ad0b6802278
Remon7 <85322997+Remon7@users.noreply.github.com>
2023-10-22 23:06:04 +0300

    Add files via upload

---
+++ cfg.c
@@ -0,0 +1,14 @@
+/*
+ * cfg.c
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: remon
+ */
+
+#include "Dio.h"
+
+
+Port_Def* PORTA = ((Port_Def*)0x39);
+Port_Def* PORTB = ((Port_Def*)0x36);
+Port_Def* PORTC = ((Port_Def*)0x33);
+Port_Def* PORTD = ((Port_Def*)0x30);
